d884b830e603c011ac5f7ee6f02b800d0c2d1b4b
Mario Amelung <mario.amelung@gmx.de>
2007-06-27 14:19:05 +0000

    Addd comment.

--- migration/change_ecaab_types.py
+++ migration/change_ecaab_types.py
@@ -1,3 +1,14 @@
+# -*- coding: UTF-8 -*-
+# $Id$
+#
+# Copyright (c) 2007 Otto-von-Guericke-Universität Magdeburg
+#
+# This file is part of ECAutoAssessmentBox.
+
+"""
+Migration from unreleased version; for internal use only.
+"""
+
 # external method
 import time
 import types
