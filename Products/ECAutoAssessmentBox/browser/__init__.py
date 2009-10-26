530c7af8f254006c433ed36e458159ff1af5192f
Mario Amelung <mario.amelung@gmx.de>
2009-10-26 21:41:03 +0000

    Uniform header.

--- Products/ECAutoAssessmentBox/browser/__init__.py
+++ Products/ECAutoAssessmentBox/browser/__init__.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+# $Id:__init__.py 1313 2009-09-28 07:03:29Z amelung $
+#
+# Copyright (c) 2006-2009 Otto-von-Guericke University Magdeburg
+#
+# This file is part of ECAutoAssessmentBox.
+#
+__author__ = """Mario Amelung <mario.amelung@gmx.de>"""
+__docformat__ = 'plaintext'
+__version__   = '$Revision:1313 $'
+
 # make me a python module
