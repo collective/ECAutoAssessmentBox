3a199525b08703cc71aa26a650ffc483d79cba50
Mario Amelung <mario.amelung@gmx.de>
2009-10-26 21:39:04 +0000

    New header; no GPL licence text.

--- Products/ECAutoAssessmentBox/content/interfaces.py
+++ Products/ECAutoAssessmentBox/content/interfaces.py
@@ -1,32 +1,15 @@
 # -*- coding: utf-8 -*-
 # $Id$
 #
-# Copyright (c) 2006-2008 Otto-von-Guericke-Universität Magdeburg
+# Copyright (c) 2006-2009 Otto-von-Guericke-Universität Magdeburg
 #
 # This file is part of ECAutoAssessmentBox.
-#
-# ECAutoAssessmentBox is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# ECAutoAssessmentBox is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with ECAutoAssessmentBox; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 __author__ = """Mario Amelung <mario.amelung@gmx.de>"""
 __docformat__ = 'plaintext'
 __version__   = '$Revision$'
 
 from zope.interface import Interface
-
-##code-section HEAD
-##/code-section HEAD
 
 class IECAutoAssessmentBox(Interface):
     """Marker interface for .ECAutoAssessmentBox.ECAutoAssessmentBox
@@ -35,6 +18,3 @@ class IECAutoAssessmentBox(Interface):
 class IECAutoAssignment(Interface):
     """Marker interface for .ECAutoAssignment.ECAutoAssignment
     """
-
-##code-section FOOT
-##/code-section FOOT
